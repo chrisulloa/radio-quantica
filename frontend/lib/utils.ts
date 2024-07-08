9e0d17ab5f9f3a0ce32b14148590306d25552ae0
Christian Gonzalez <chrisulloago@gmail.com>
2024-07-07 17:10:58 -0700

    Fixing URL

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,5 +1,5 @@
 const serverBaseURL =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV !== "development"
     ? "https://squid-app-svk3v.ondigitalocean.app"
     : process.env.SERVER_BASE_URL || "http://localhost:3000";
 
