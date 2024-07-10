f0efc83cb3be824cc578b0330e793f6bb9090880
Christian Gonzalez <chrisulloago@gmail.com>
2024-07-09 22:43:25 -0700

    schedule

--- frontend/pages/schedule.tsx
+++ frontend/pages/schedule.tsx
@@ -1,12 +1,111 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { weekInfo } from "../lib/testData";
+import { weekInfo } from "../lib/mockData";
+import { DateTime } from "luxon";
+import { useCallback, useState } from "react";
+
+type Day =
+  | "sunday"
+  | "monday"
+  | "tuesday"
+  | "wednesday"
+  | "thursday"
+  | "friday"
+  | "saturday";
+
+const days: Day[] = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+interface DaySchedule {
+  start_timestamp: string;
+  end_timestamp: string;
+  name: string;
+  id: number;
+  instance_id: number;
+  record: number;
+  url: string;
+  starts: string;
+  ends: string;
+}
+const formatTime = (time: string) =>
+  DateTime.fromFormat(time, "yyyy-MM-dd HH:mm:ss").toFormat("HH:mm");
+
+const DaysSelector = ({
+  currentDay,
+  onClick,
+}: {
+  currentDay: Day;
+  onClick: (e: any) => void;
+}) => {
+  return (
+    <div className="flex gap-x-2 sm:gap-x-4">
+      {days.map((day) => {
+        return (
+          <button
+            key={day}
+            className={`capitalize px-3 py-1 text-white border ${
+              day === currentDay ? "bg-white text-black" : ""
+            }`}
+            onClick={onClick}
+            value={day}
+          >
+            {day}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+const DaySchedule = ({ schedule }: { schedule: DaySchedule[] }) => {
+  return (
+    <table className="flex text-white w-full">
+      <thead></thead>
+      <tbody>
+        {schedule.map((item, i) => {
+          const start = formatTime(item.start_timestamp);
+          const end = formatTime(item.end_timestamp);
+          return (
+            <tr key={i}>
+              <td className="pr-5">
+                {start} - {end}
+              </td>
+              <td>{item.name}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 export default function SchedulePage() {
   const router = useRouter();
 
+  const data = weekInfo;
+
+  const monday = weekInfo.monday;
+  const today = new Date().getDay();
+
+  const [day, setDay] = useState<Day>(days[today]);
+  const [schedule, setSchedule] = useState<DaySchedule[]>(data[day]);
+  const onClick = useCallback(
+    (e: any) => {
+      setDay(e.target.value as Day);
+      setSchedule(data[e.target.value as Day]);
+    },
+    [data]
+  );
+
   return (
-    <div className="mb-8">
+    <div className="mb-8 text-white">
       <Head>
         <title>Schedule - Rádio Quântica</title>
         <meta name="twitter:card" content="summary_large_image" />
@@ -45,6 +144,32 @@ export default function SchedulePage() {
         ></meta>
         <meta property="og:image:type" content="image/jpg"></meta>
       </Head>
+      <div className="sm:flex sm:flex-col sm:items-start sm:mx-8 sm:w-min hidden">
+        <h1 className="pb-2 text-xl">Schedule</h1>
+        <hr className="w-full"></hr>
+        <div className=" w-fit">
+          <div className="py-4 text-lg w-full">
+            <DaysSelector currentDay={day} onClick={onClick}></DaysSelector>
+          </div>
+          <div className="w-full flex">
+            <DaySchedule schedule={schedule}></DaySchedule>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col sm:hidden text-xs mx-4">
+        <h1 className="pb-2 text-xl">Schedule</h1>
+        <hr className="w-full"></hr>
+        <div className="pt-4">
+          {days.map((d) => {
+            return (
+              <div className="pb-2" key={d}>
+                <p className="underline pb-1 pt-4">{d.toUpperCase()}</p>
+                <DaySchedule schedule={data[d]}></DaySchedule>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
