f31ae3c8295e313c3c96afbe2448bcfa7fea44ad
Christian Gonzalez <chrisulloago@gmail.com>
2024-07-26 18:20:40 -0700

    Update utils.ts

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,6 @@
 const serverBaseURL =
   process.env.NODE_ENV !== "development"
-    ? "https://squid-app-svk3v.ondigitalocean.app"
+    ? "https://api.radioquantica.com"
     : process.env.SERVER_BASE_URL || "http://localhost:3000";
 
 export default serverBaseURL;
