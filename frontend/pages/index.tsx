7e8c78a2216cae099ec56ef3ff7d3efea4fbd4bb
Christian Gonzalez <chrisulloago@gmail.com>
2024-07-27 15:40:50 -0700

    Hide last child

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -247,7 +247,7 @@ export default function Home({
         </div>
         <hr className="mt-4"></hr>
         <div className="mb-8 justify-center flex mt-5">
-          <div className="gap-5 grid grid-cols-1 lg:grid-cols-2 w-full">
+          <div className="gap-5 grid grid-cols-1 lg:grid-cols-2 w-full max-lg:[&>iframe:last-child]:hidden">
             <LiveVideos liveVideos={liveVideos}></LiveVideos>
           </div>
         </div>
