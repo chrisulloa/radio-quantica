d4799698f0ce0818b0a3fb835240e6f611e94a00
Christian Gonzalez <chrisulloago@gmail.com>
2025-01-29 08:29:57 -0800

    Update eslint

---
+++ api/eslint.config.mjs
@@ -0,0 +1,74 @@
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ['build/*', 'dist/*', 'eslint.config.mjs'],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      'prettier'
+    )
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      prettier,
+    },
+    languageOptions: {
+      globals: {
+        ...Object.fromEntries(
+          Object.entries(globals.browser).map(([key]) => [key, 'off'])
+        ),
+        ...globals.node,
+      },
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        tsconfigRootDir: '.',
+        project: './tsconfig.json',
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {},
+        node: {
+          extensions: ['.ts', '.js', '.tsx'],
+          typescript: {
+            alwaysTryTypes: true,
+            paths: './tsconfig.json',
+          },
+        },
+      },
+    },
+    rules: {
+      'no-console': 'off',
+      'prettier/prettier': 'error',
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      '@typescript-eslint/no-restricted-types': 'warn',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+];
--- api/src/access/isAdmin.ts
+++ api/src/access/isAdmin.ts
@@ -1,6 +1,7 @@
 import { Access, FieldAccess } from 'payload/types';
 import { User } from '../types/payload';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const isAdmin: Access<any, User> = ({ req: { user } }) =>
   // Return true or false based on if the user has an admin role
   Boolean(user?.roles?.includes('admin'));
