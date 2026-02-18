241adda266e25711ed821da2dbedc35eb520609e
Christian Gonzalez <chrisulloago@gmail.com>
2026-02-17 21:56:16 -0800

    Updating formatting for video

--- frontend/components/liveVideoCard.tsx
+++ frontend/components/liveVideoCard.tsx
@@ -1,8 +1,8 @@
 const LiveVideoCard = () => {
   return (
-    <div className="w-full max-w-4xl mx-auto p-4 aspect-video">
+    <div className="w-full max-w-4xl mx-auto md:p-4 md:-mb-16">
       <iframe
-        className="w-full h-full aspect-video"
+        className="w-full aspect-video"
         src="https://owncast.radioquantica.com/embed/video"
         title="Owncast"
         referrerPolicy="origin"
