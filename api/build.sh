c6234a5ea98d5bf4974a5a1d2d735d626edc445a
Christian Gonzalez <chrisulloago@gmail.com>
2026-08-23 04:24:52 -0700

    Fixing build issues

--- api/build.sh
+++ api/build.sh
@@ -2,6 +2,8 @@
 
 npm install --only=dev
 export PAYLOAD_CONFIG_PATH=src/payload.config.ts
+echo "Generating import map..."
+npx payload generate:importmap
 echo "Running payload build..."
 npx next build
 echo "Running tsc..."
