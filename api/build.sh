6c56c5e1cc63c016d8c0cf345b6f56e8a1d318c7
Christian Gonzalez <chrisulloago@gmail.com>
2026-09-03 21:17:07 -0700

    Updating packages and updating build

--- api/build.sh
+++ api/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-npm install --only=dev
 export PAYLOAD_CONFIG_PATH=src/payload.config.ts
 echo "Generating import map..."
 npx payload generate:importmap
 echo "Running payload build..."
 npx next build
-echo "Running tsc..."
-tsc -p tsconfig.prod.json
